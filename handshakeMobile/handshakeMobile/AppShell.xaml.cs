c9add6197c874f8256fca0c57939761a165d1f7b
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2020-09-29 20:20:20 +0200

    android app added

---
+++ handshakeMobile/handshakeMobile/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using handshakeMobile.ViewModels;
+using handshakeMobile.Views;
+using Xamarin.Forms;
+
+namespace handshakeMobile
+{
+  public partial class AppShell : Xamarin.Forms.Shell
+  {
+    public AppShell()
+    {
+      InitializeComponent();
+      Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+      Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+    }
+
+    private async void OnMenuItemClicked(object sender, EventArgs e)
+    {
+      await Shell.Current.GoToAsync("//LoginPage");
+    }
+  }
+}
